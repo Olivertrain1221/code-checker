b483e847552689062c21638a109aa2a85f80f498
Olivertrain1221 <olivertrain1996@gmail.com>
2022-01-11 01:12:24 +0000

    Add error message if api ky incorrect

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
-uh2yF8F_dCn_YEHWnzipVSYQO9Y
+const API_KEY = "uh2yF8F_dCn_YEHWnzipVSYQO9Y";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+    //Gets the api with the URL and the KEY
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+    // actually fetches the URL
+    const response = await fetch(queryString);
+    // waits for the response and puts it in json format (javscript object)
+    const data = await response.json();
+    // if the response is correct ie status 200
+    if (response.ok) {
+        console.log(data.expiry);
+    } else {
+        throw new Error(data.error);
+    }
+}
